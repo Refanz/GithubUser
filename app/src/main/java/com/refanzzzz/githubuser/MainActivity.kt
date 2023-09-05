cdfe149c64d0c24d3b8233adaec65cf173167608
Refanzzzz <refandasuryasaputra@gmail.com>
2023-09-06 01:35:30 +0700

    first commit

---
+++ app/src/main/java/com/refanzzzz/githubuser/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.refanzzzz.githubuser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.refanzzzz.githubuser.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            svGithubUser.setupWithSearchBar(sbGithubUser)
+            svGithubUser
+                .editText
+                .setOnEditorActionListener { textView, actionId, event ->
+                    sbGithubUser.text = svGithubUser.text
+                    svGithubUser.hide()
+                    Toast.makeText(this@MainActivity, svGithubUser.text, Toast.LENGTH_SHORT).show()
+                    false
+                }
+        }
+    }
+}
